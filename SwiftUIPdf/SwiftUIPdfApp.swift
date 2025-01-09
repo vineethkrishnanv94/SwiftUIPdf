44b55449bad9f597f5bb887839e6dc2902e4d9c6
Vineeth Krishnan V <vineethkrishnanv94@gmail.com>
2025-01-09 18:46:39 -0500

    Initial Commit

---
+++ SwiftUIPdf/SwiftUIPdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPdfApp.swift
+//  SwiftUIPdf
+//
+//  Created by Vineeth Krishnan V on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIPdfUITests/SwiftUIPdfUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUIPdfUITestsLaunchTests.swift
+//  SwiftUIPdfUITests
+//
+//  Created by Vineeth Krishnan V on 09/01/25.
+//
+
+import XCTest
+
+final class SwiftUIPdfUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
