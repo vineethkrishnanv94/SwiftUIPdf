d07f7171f17a9cf6e6de2a7c3ed8b320667d8c4d
Vineeth Krishnan V <vineethkrishnanv94@gmail.com>
2025-01-09 18:50:22 -0500

    Initial commit

--- SwiftUIPdf/ContentView.swift
+++ SwiftUIPdf/ContentView.swift
@@ -9,14 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ShareLink("Export Pdf", item: render())
     }
+    
+    @MainActor func render() -> URL {
+            let renderer = ImageRenderer(content:
+                                            PDFView()
+            )
+
+            let url = URL.documentsDirectory.appending(path: "output.pdf")
+
+            renderer.render { size, context in
+  
+                var box = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+
+                guard let pdf = CGContext(url as CFURL, mediaBox: &box, nil) else {
+                    return
+                }
+
+                pdf.beginPDFPage(nil)
+                context(pdf)
+                pdf.endPDFPage()
+                pdf.closePDF()
+            }
+
+            return url
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ SwiftUIPdf/PDFView.swift
@@ -0,0 +1,26 @@
+//
+//  PDFView.swift
+//  SwiftUIPdf
+//
+//  Created by Vineeth Krishnan V on 09/01/25.
+//
+
+import SwiftUI
+
+struct PDFView: View {
+    var body: some View {
+        VStack {
+            
+            Image("swiftIcon")
+            
+            Text("Hello, World!")
+                .font(.largeTitle)
+        }
+    }
+}
+
+struct PDFView_Previews: PreviewProvider {
+    static var previews: some View {
+        PDFView()
+    }
+}
